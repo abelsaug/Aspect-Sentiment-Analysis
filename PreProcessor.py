983a8faaa40efbb379fe62f2c414d379e8fafe6a
phil7394 <philipthomas7394@gmail.com>
2018-04-07 17:52:02 -0500

    fixed autocorrect

--- PreProcessor.py
+++ PreProcessor.py
@@ -1,12 +1,10 @@
-import pandas
-import re
-import enchant
-from collections import Counter
-import difflib
-from autocorrect import spell
-from nltk.corpus import stopwords
-import nltk
 import argparse
+import re
+import autocorrect
+import nltk
+import pandas
+import enchant
+from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
 
@@ -15,12 +13,10 @@ def split_text(df):
     cols = list(df)
     sdf = pandas.DataFrame(columns=cols)
     for index, row in df.iterrows():
-        # print row
         if re.search(r'\d+_\d+', row[' text']) is not None:
             typo_list = row[' text'].split('\n')
             row_list = typo_list[0].split(',')
             row_list[0] = clean_string(row_list[0])
-            # print [row[cols[0]]] + row_list
             sdf.loc[len(sdf.index)] = [row[cols[0]]] + row_list
             for t in typo_list[1:-1]:
                 row_list = t.split(',')
@@ -37,9 +33,33 @@ def split_text(df):
 
 def clean_string(string):
     string = re.sub("\[comma]", ',', string)
-    string = re.sub(r'[^a-zA-Z0-9-\'\.$!?, ]', '', string)
+    string = re.sub(r'[^a-zA-Z0-9-\'\.$!?/%, ]', ' ', string)
+    # string = re.sub(r'[()\"\[\]_]', ' ', string)
     string = re.sub(r'\s+', ' ', string)
     return string.strip()
+
+
+def remove_punc(pudf):
+    print "Removing all punctuations..."
+    cols = list(pudf)
+    punc_df = pandas.DataFrame(columns=cols)
+    for index, row in pudf.iterrows():
+        punc_row = [row[c] for c in cols]
+        punc_row[1] = re.sub(r'([^a-zA-Z0-9$%\' ])', ' ', punc_row[1])
+        punc_row[1] = re.sub(r'\s+', ' ', punc_row[1]).strip()
+        punc_df.loc[len(punc_df.index)] = punc_row
+    return punc_df
+
+
+def lower_case(pudf):
+    print "Converting to lowercase..."
+    cols = list(pudf)
+    punc_df = pandas.DataFrame(columns=cols)
+    for index, row in pudf.iterrows():
+        punc_row = [row[c] for c in cols]
+        punc_row[1] = punc_row[1].lower()
+        punc_df.loc[len(punc_df.index)] = punc_row
+    return punc_df
 
 
 def auto_correct(adf):
@@ -48,43 +68,68 @@ def auto_correct(adf):
     acdf = pandas.DataFrame(columns=cols)
     for index, row in adf.iterrows():
         acdf_row = [row[c] for c in cols]
-        print row['example_id']
         acdf_row[1] = get_correct_spelling(row[' text'])
-        # acdf_row[2] = get_correct_spelling(row[' aspect_term'])
         acdf.loc[len(acdf.index)] = acdf_row
     return acdf
 
 
+def segment_str(chars, exclude=None):
+    words = []
+    if not chars.isalpha():
+        return [chars]
+    if not exclude:
+        exclude = set()
+
+    working_chars = chars
+    while working_chars:
+        for i in range(len(working_chars), 1, -1):
+            segment = working_chars[:i]
+            if eng_dict.check(segment) and segment not in exclude:
+                words.append(segment)
+                working_chars = working_chars[i:]
+                break
+        else:
+            if words:
+                exclude.add(words[-1])
+                return segment_str(chars, exclude=exclude)
+            return [chars]
+    return words
+
+
 def get_correct_spelling(string):
-    string = re.sub(r'([^a-zA-Z-\' ])', r' \1', string)
+    string = re.sub(r'([^a-zA-Z0-9-\'$%/ ])', r' \1', string)
     string = re.sub(r'\s+', ' ', string)
     w_list = string.strip().split(' ')
-    # print w_list
+    correct_w_list = []
     tagged_list = nltk.pos_tag(w_list)
-    # print tagged_list
     for i in range(len(w_list)):
-        lower_w = w_list[i].lower()
-        if w_list[i].isalpha() and my_dict.check(lower_w) is False:
-            if tagged_list[i][1] not in ['NNP', 'NNPS']:
-                # a = my_dict.suggest(w_list[i])
-                # best_ratio = 0.0
-                # best_words = []
-                # for b in a:
-                #     tmp = difflib.SequenceMatcher(None, w_list[i], b).ratio()
-                #     if tmp > best_ratio:
-                #         best_words = [b]
-                #         best_ratio = tmp
-                #     elif tmp == best_ratio:
-                #         best_words.append(b)
-                # print w_list[i], tagged_list[i][1], best_words
-                # w_list[i] = best_words[0]  # TODO -- choose most relevant word
-                # print w_list[i], spell(w_list[i])
-                w_list[i] = spell(w_list[i])
-    string = ' '.join(w_list)
-    string = re.sub(r' ([^a-zA-Z-\' ])', r'\1', string)
+        if w_list[i].isalpha():
+            if check_in_dict(w_list[i]) is False:
+                if len(w_list[i]) >= 2 and w_list[i][0].isupper() and w_list[i][1].islower():
+                    correct_w_list.append(w_list[i])
+                elif tagged_list[i][1] not in ['NNP', 'NNPS']:
+                    if autocorrect.spell(w_list[i]) == w_list[i]:
+                        seg_list = segment_str(w_list[i])
+                        correct_w_list += map(lambda w: autocorrect.spell(w), seg_list)
+                    else:
+                        correct_w_list.append(autocorrect.spell(w_list[i]))
+                else:
+                    correct_w_list.append(autocorrect.spell(w_list[i]))
+            else:
+                correct_w_list.append(w_list[i])
+        else:
+            correct_w_list.append(w_list[i])
+
+    string = ' '.join(correct_w_list)
+    string = re.sub(r' ([^a-zA-Z0-9-\'$/% ])', r'\1', string)
     string = re.sub(r'\s+', ' ', string)
 
     return string
+
+
+def check_in_dict(wrd):
+    match_set = (autocorrect.common([wrd]) or autocorrect.exact([wrd]) or autocorrect.known([wrd]))
+    return bool(match_set)
 
 
 def remove_stop_words(stdf):
@@ -103,9 +148,8 @@ def remove_stop_words(stdf):
             else:
                 filtered_text.append(w)
         join_text = ' '.join(filtered_text)
-        # print row[' text']
         rsw_row[1] = re.sub(r' ([^a-zA-Z-\' ])', r'\1', join_text)
-        # print rsw_row[1]
+
         rsw_df.loc[len(rsw_df.index)] = rsw_row
     return rsw_df
 
@@ -140,7 +184,6 @@ def remove_proper_nouns(pndf):
         pnw_text = re.sub(r'([^a-zA-Z-\' ])', r' \1', pnw_row[1])
         pnw_text = re.sub(r'\s+', ' ', pnw_text).strip()
         pnw_word_list = pnw_text.split(' ')
-        # print pnw_word_list
         tagged_list = nltk.pos_tag(pnw_word_list)
         rm_pn_list = []
         for i in range(len(pnw_word_list)):
@@ -161,35 +204,27 @@ if __name__ == '__main__':
     required = parser.add_argument_group('required arguments')
     required.add_argument('-i', '--input', help='path to input training data file', required=True)
     required.add_argument('-o', '--output', help='path to output processed data file', required=True)
-    optional.add_argument('-m', '--model', help='training model(Naive Baye\'s)', required=False)  #TODO reqd. model-feature engg.
     optional.add_argument('-s', '--stopwords', help='remove stopwords(y/n)', choices=['y', 'n'], required=False)
-    optional.add_argument('-p', '--stemwords', help='porter-stem words(y/n)', choices=['y', 'n'], required=False)
+    optional.add_argument('-r', '--stemwords', help='porter-stem words(y/n)', choices=['y', 'n'], required=False)
     optional.add_argument('-n', '--propernouns', help='remove propernouns(y/n)', choices=['y', 'n'], required=False)
+    optional.add_argument('-p', '--punc', help='remove punctuations(y/n)', choices=['y', 'n'], required=False)
+    optional.add_argument('-l', '--lowercase', help='to lowercase(y/n)', choices=['y', 'n'], required=False)
+
     parser._action_groups.append(optional)
     args = vars(parser.parse_args())
 
-    en_dict = enchant.DictWithPWL("en_US")
     ps = PorterStemmer()
     df = pandas.read_csv(args['input'])
+    eng_dict = enchant.Dict("en_US")
+
     df = split_text(df)
 
-    text_set = set(df[' text'].tolist())
-    text_str = ' '.join(text_set).lower()
-    text_str = re.sub(r'[^a-zA-Z-\' ]', '', text_str)
-    text_str = re.sub(r'\s+', ' ', text_str)
-    word_list = text_str.strip().split(' ')
-    word_freqs = Counter(word_list)
-    # print word_freqs
-    f = open('pwl_dict.txt', 'wr+')
-    f.truncate()
-    f.close()
-    my_file = open('pwl_dict.txt', 'a')
-    for word in word_freqs:
-        if word_freqs[word] > 3 and en_dict.check(word) is False:
-            my_file.write(word + "\n")
-    my_file.close()
-    my_dict = enchant.DictWithPWL("en_US", "pwl_dict.txt")
-
+    with open("include_dict.txt") as inc_file:
+        for line in inc_file:
+            autocorrect.word.KNOWN_WORDS.add(line.rstrip('\n'))
+    with open("exclude_dict.txt") as exd_file:
+        for line in exd_file:
+            autocorrect.word.KNOWN_WORDS.remove(line.rstrip('\n'))
     df = auto_correct(df)
 
     # OPTIONALS
@@ -199,6 +234,10 @@ if __name__ == '__main__':
         df = stem_words(df)
     if args['propernouns'] == 'y':
         df = remove_proper_nouns(df)
+    if args['punc'] == 'y':
+        df = remove_punc(df)
+    if args['lowercase'] == 'y':
+        df = lower_case(df)
 
     df.to_csv(args['output'], sep='\t')
 
