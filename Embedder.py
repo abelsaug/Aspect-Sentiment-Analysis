030932dfc9187ef90fd61483ed6baa2fa4c00c21
abelsaug <asa.11ec05@nitk.edu.in>
2018-05-03 02:21:28 -0500

    Updated Embeddings. added position, changed to to tfidf embedder, and added idv

--- Embedder.py
+++ Embedder.py
@@ -2,6 +2,7 @@ import argparse
 import numpy as np
 import nltk
 import pandas
+from gensim.models import KeyedVectors
 from gensim.models import Word2Vec
 import spacy
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -52,14 +53,13 @@ class TfidfEmbeddingVectorizer(object):
                          for w in X if w in self.word2vec] or
                         [np.zeros(self.dim)], axis=0)
             ])
-
-
+    
 def pos2vec(pvdf):
     with open(args['output'] + "/pos2vec.txt", 'w') as of:
         full_pos_list = []
         for index, row in pvdf.iterrows():
             word_list = nltk.word_tokenize(row[' text'])
-            tagged_list = map(lambda w_p_tuple: w_p_tuple[1], nltk.pos_tag(word_list))
+            tagged_list = list(map(lambda w_p_tuple: w_p_tuple[1], nltk.pos_tag(word_list)))
             full_pos_list.append(tagged_list)
         model = Word2Vec(full_pos_list, size=120, window=6, min_count=1, workers=4)
         w2v = dict(zip(model.wv.index2word, model.wv.syn0))
@@ -70,13 +70,14 @@ def pos2vec(pvdf):
             tagged_list = map(lambda w_p_tuple: w_p_tuple[1], nltk.pos_tag(word_list))
             vec = mev.transform(tagged_list)
             p2v_dict[row['example_id']] = " ".join(map(str, vec.tolist()))
-        for k, v in p2v_dict.iteritems():
+        for k, v in p2v_dict.items():
             of.write(k + " " + v + "\n")
+        return p2v_dict
 
 
 def lex2vec(lvdf):
     sswe_dict = dict()
-    with open("embedding/sswe-u.txt", 'r') as sf:
+    with open("embedding/sswe-u.txt", 'r', encoding = 'utf8') as sf:
         for line in sf:
             values = line.split()
             word = values[0]
@@ -84,14 +85,16 @@ def lex2vec(lvdf):
             sswe_dict[word] = coefs
 
     with open(args['output'] + "/lex2vec.txt", 'w') as of:
-        mev = MeanEmbeddingVectorizer(sswe_dict)
+        tev = TfidfEmbeddingVectorizer(sswe_dict)
         l2v_dict = dict()
         for index, row in lvdf.iterrows():
             word_list = nltk.word_tokenize(row[' text'])
-            vec = mev.transform(word_list)
+            tev.fit(word_list)
+            vec = tev.transform(word_list)[0]
             l2v_dict[row['example_id']] = " ".join(map(str, vec.tolist()))
-        for k, v in l2v_dict.iteritems():
+        for k, v in l2v_dict.items():
             of.write(k + " " + v + "\n")
+        return l2v_dict
 
 def sentence2sequence(glove_wordmap, sentence):
     """
@@ -145,19 +148,22 @@ def glove2vec(gvdf, customTrained=False, mean = False):
         g2v_dict = dict()
         for index, row in gvdf.iterrows():
             word_list = row[' text'].split(" ")
-
+            
             if mean:
                 tev.fit(word_list)
                 word_list = tev.transform(word_list)[0]
-            g2v_dict[row['example_id']] = " ".join(map(str, word_list.tolist()))
+            else:
+                word_list, words = sentence2sequence(glove_wordmap, row[' text'])
+            g2v_dict[row['example_id']] = " ".join(map(str, word_list))
         for k, v in g2v_dict.items():
             of.write(k + " " + v + "\n")
+        return g2v_dict
         
 
 def word2vec(wdf, customTrained=False, mean = False):
     # Load Google's pre-trained Word2Vec model.
     if not customTrained:
-        w2v_wordmap = gensim.models.Word2Vec.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)  
+        w2v_wordmap = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)  
     word_list = []
     sentences = []
     if customTrained:
@@ -178,13 +184,53 @@ def word2vec(wdf, customTrained=False, mean = False):
             if mean:
                 tev.fit(word_list)
                 word_list = tev.transform(word_list)[0]
-            w2v_dict[row['example_id']] = " ".join(map(str, word_list.tolist()))
+            w2v_dict[row['example_id']] = " ".join(map(str, word_list))
         for k, v in w2v_dict.items():
             of.write(k + " " + v + "\n")
+        return w2v_dict
             
-# def postion2vec(df):
+def postion2vec(df):
+    with open(args['output'] + "/position2vec.txt", 'w') as of:
+        p2v_dict = dict()
+        for index, row in df.iterrows():
+            word_text = nltk.word_tokenize(row[' text'])
+            aspect_terms = nltk.word_tokenize(row[' aspect_term'])
+            aspect_index = []
+            for i, word in enumerate(word_text):
+                if word in aspect_terms:
+                    aspect_index.append(i)
+            aspect_index.sort()
+            word_list = []
+            for i, word in enumerate(word_text):
+                if i < aspect_index[0]:
+                    word_list.append(aspect_index[0] - i)
+                elif i > aspect_index[-1]:
+                    word_list.append(i - aspect_index[-1])
+                else:
+                    word_list.append(0)
+            p2v_dict[row['example_id']] = " ".join(map(str, word_list))
+        for k, v in p2v_dict.items():
+            of.write(k + " " + v + "\n")
+        return p2v_dict
     
-    
+def idv(df):
+    with open(args['output'] + "/improvedvec.txt", 'w') as of:
+        idv_dict = dict()
+        pos_dict = pos2vec(df)
+        lex_dict = lex2vec(df)
+        mg2v_dict = glove2vec(df, customTrained=False, mean = True)
+        g2v_dict = glove2vec(df, customTrained=False, mean = False)
+        w2v_dict = word2vec(df, customTrained=False, mean = False)
+        cmw2v_dict = word2vec(df, customTrained=True, mean = True)
+        mw2v_dict = word2vec(df, customTrained=False, mean = True)
+        position_dict = postion2vec(df)
+        for index, row in df.iterrows():
+            idv_dict[row['example_id']] = mg2v_dict[row['example_id']] + " " + position_dict[row['example_id']]  + " " + lex_dict[row['example_id']] + " " + pos_dict[row['example_id']]
+        for k, v in idv_dict.items():
+            of.write(k + " " + v + "\n")
+        
+
+
     
 def fit_to_size(matrix, shape):
     res = np.zeros(shape)
@@ -212,7 +258,9 @@ if __name__ == '__main__':
     optional.add_argument('-cgv', '--customglove2vec', help='custom glove2vec (y/n)', choices=['y', 'n'], required=False)
     optional.add_argument('-mcwv', '--meancustomword2vec', help='mean custom word2vec (y/n)', choices=['y', 'n'], required=False)
     optional.add_argument('-mcgv', '--meancustomglove2vec', help='mean custom glove2vec (y/n)', choices=['y', 'n'], required=False)
-    
+    optional.add_argument('-p2v', '--position2vec', help='position2vec (y/n)', choices=['y', 'n'], required=False)
+    optional.add_argument('-idv', '--improvedvec', help='improvedvec (y/n)', choices=['y', 'n'], required=False)
+
     parser._action_groups.append(optional)
     args = vars(parser.parse_args())
 
@@ -237,4 +285,9 @@ if __name__ == '__main__':
         word2vec(df, customTrained=True, mean = True)
     if args['meancustomglove2vec'] == 'y':
         glove2vec(df, customTrained=True, mean = True)
+    if args['position2vec'] == 'y':
+        postion2vec(df)
+    if args['improvedvec'] == 'y':
+        idv(df)
+        
 
